522adcc6d9b1c67ba1a8b0492380a6f45b79b19c
crisleo94 <cristhian.reinoso@live.com>
2021-04-23 02:41:44 -0500

    add basic CRUD structure using SpringBoot, use JPA to connect to a MySQL database

---
+++ src/main/java/com/reinoso/apicrud/repository/ReporteServicioRepository.java
@@ -0,0 +1,7 @@
+package com.reinoso.apicrud.repository;
+
+import com.reinoso.apicrud.entity.ReporteServicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReporteServicioRepository extends JpaRepository<ReporteServicio, Integer> {
+}
